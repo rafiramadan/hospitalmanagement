5f5d8b885812efcdb4a385d447bae087ca6db195
github_username <email_address>
2022-12-01 15:21:04 +0700

    Rubah tampilan form detail triase

--- resources/views/main/datapasien.blade.php
+++ resources/views/main/datapasien.blade.php
@@ -40,7 +40,7 @@
                 </strong>
                 <div class="pull-right">
                     <a href="{{ url('datapasien/tambahpasien')}} " class="btn btn-success btn-sm">
-                        <i class="fa fa-plus"></i> Tambah Data
+                        <i class="fa fa-plus-circle"></i> Tambah Data
                     </a>
                 </div>
             </div>
@@ -83,11 +83,11 @@
                                 <td>{{ $item->pekerjaan }} </td>
                                 <td>{{ $item->jenisbayar }} </td>
                                 <td class="text-center">
-                                     <a href="{{ url('datapasien/editpasien/' .$item->id)}}" class="btn btn-primary btn-sm"> <i class="fa fa-pencil" ></i></a> 
+                                     <a href="{{ url('datapasien/editpasien/' .$item->id)}}" class="btn btn-primary btn-sm"> <i class="fa fa-pencil" ></i> Edit</a> 
                                      <form action="{{ url('datapasien/' .$item->id)}}" method="POST" class="d-inline" onsubmit="return confirm('Yakin hapus data?')">
                                         @method('delete')
                                         @csrf
-                                        <button class="btn btn-danger btn-sm"> <i class="fa fa-trash"></i> </button>
+                                        <button class="btn btn-danger btn-sm"> <i class="fa fa-trash"></i> Hapus</button>
                                      </form>
                                 </td>
                             </tr>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -32,10 +32,10 @@
                     <label><i class="fa fa-key"></i> Password</label>
                     <input type="password" name="password" class="form-control" placeholder="Password" required="">
                 </div>
-                <div class="form-group">
+                <!-- <div class="form-group">
                     <label><i class="fa fa-address-book"></i> Role</label>
                     <input type="text" name="role" class="form-control" value="Guest" readonly>
-                </div>
+                </div> -->
                 <button type="submit" class="btn btn-primary btn-block"><i class="fa fa-user"></i> Register</button>
                 <hr>
                 <p class="text-center">Sudah punya akun silahkan <a href="{{('login')}}">Login Disini!</a></p>
