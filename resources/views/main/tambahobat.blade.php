b9d90574f07ac3a3fef37b83f5a06eb26b556d87
rafiramadan <rafiramadan.space@gmail.com>
2022-12-05 11:11:06 +0700

    fix merge

--- resources/views/main/tambahobat.blade.php
+++ resources/views/main/tambahobat.blade.php
@@ -58,39 +58,6 @@
                             </div>
                             <button type="submit" class="btn btn-success">Simpan Data</button>
                         </form>
-
-            <div class="card-header">
-                    <strong>
-                        Tambah Data Obat
-                    </strong>
-                    <div class="pull-right">
-                        <a href="{{ url('dataobat')}} " class="btn btn-success btn-sm">
-                            <i class="fa fa-undo"></i> Kembali
-                        </a>
-                    </div>
-                </div>
-                <div class="card-body table-responsive">
-                    <div class="row">
-                        <div class="col-md-4 offset-md-4">
-                            <form action="{{ url('dataobat')}} " method="post">
-                                @csrf
-                                <div class="form-group">
-                                    <label>Kode Obat</label>
-                                    <input type="text" name="kode" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <label>Nama</label>
-                                    <input type="text" name="nama" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <label>Stok</label>
-                                    <input type="text" name="stok" class="form-control" required>
-                                </div>
-                                <button type="submit" class="btn btn-success">Simpan Data</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
--- resources/views/main/tambahrujuk.blade.php
+++ resources/views/main/tambahrujuk.blade.php
@@ -27,22 +27,10 @@
 
     <div class="animated fadeIn">
         <div class="card">
-<<<<<<< HEAD
             <div class="card-header">
                 <strong>
                     Tambah Data Rujuk RS lain
                 </strong>
-=======
-            <div class="pull-left">
-                <div class=" offset-md-4">
-                    <br>
-                    <strong>
-                        <h2>
-                            Tambah Data Rujuk RS lain
-                        </h2>
-                    </strong>
-                </div>
->>>>>>> dc6ebde8cf20a8b057a85190138ab90ed4953756
                 <div class="pull-right">
                     <a href="{{ url('rujuk')}} " class="btn btn-success btn-sm">
                         <i class="fa fa-undo"></i> Kembali
